4868fea714740a4c4b9a73e208f043f8b23c39d0
Nathan Wu <cnwu1@outlook.com>
2017-11-04 14:27:11 -0400

    add matching endpoint

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Endpoint `http://localhost:8080/registernewcustomer`
 Method `POST`
 
 Data format `JSON`
+
+Sample `curl -v -d "hello" http://localhost:8080/registernewcustomer`
+
+## To get matches of customers
+
+Endpoint `http://localhost:8080/getcustomermatches`
+
+Method `GET`
+
+Sample `curl -v http://localhost:8080/getcustomermatches`
---
+++ src/main/java/ca/giftthecode/indspire/mentormatcher/CustomerMatching/CustomerMatching.java
@@ -0,0 +1,20 @@
+package ca.giftthecode.indspire.mentormatcher.CustomerMatching;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by nathanwu on 11/4/17.
+ */
+
+@RestController
+public class CustomerMatching {
+
+    @RequestMapping(value = "/getcustomermatches", method = RequestMethod.GET)
+    public String registerNewCustomer() {
+        return "no matches";
+    }
+
+}
